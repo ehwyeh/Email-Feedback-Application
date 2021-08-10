5be6de0e809a57bfd1f5033a54c52e8164da14f2
Ethan Yeh <ehwyeh@ucdavis.edu>
2021-08-10 15:14:05 -0700

    Added passport.js Library

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Root file of node project
 
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
+
+// Tells passport to create a new GoogleStrategy instance
+passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
   res.send({loveYou: 'carnie'});
