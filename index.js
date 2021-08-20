b9b47f80e87a4f0a0e1b145eb9be9d9aebd90a9b
Ethan Yeh <ehwyeh@ucdavis.edu>
2021-08-19 20:56:56 -0700

    refactured server side

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
 // Root file of node project
 
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys.js");
+require('./services/passport');
 const app = express();
 
-// Tells passport to create a new GoogleStrategy instance
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
-app.get('/auth/google/callback', 
-passport.authenticate('google'))
-app.get("/", (req, res) => {
-  res.send({ loveYou: "cae" });
-});
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 // applicaition link https://rocky-oasis-53450.herokuapp.com/
